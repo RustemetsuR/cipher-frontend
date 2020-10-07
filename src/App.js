787cf8e0b18e78134cc2a7cddc2d80c0a6769eb4
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-10-07 19:36:30 +0600

    Added MainPage container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from './components/UI/Container/Container';
+import MainPage from './containers/MainPage/MainPage';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <MainPage/>
+        </Container>
     </div>
   );
 }
